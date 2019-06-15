0c4b956a8d33f84c1fa7e39bc54b787df822b243
Alex Vlad <vlad@avanticart.ro>
2019-06-15 14:47:35 +0300

    movie details on click

--- js/movie-details.js
+++ js/movie-details.js
@@ -19,7 +19,7 @@ function getMovieDataByID(movieID) {
                 if (responseData.Response == 'False') {
                     searchResults.innerHTML = '<div class="not-found-message"><p>' + responseData.Error + '</p></div>'
                 } else {                    
-                    //movieDetailsModal(moresponseData)
+                    movieDetailsModal(responseData)
                     console.log(responseData)
                 }
 
@@ -29,4 +29,57 @@ function getMovieDataByID(movieID) {
     }).catch(function (error) {
         console.log('There has been a problem with your fetch operation: ', error.message);
     })
+}
+
+
+function movieDetailsModal(data) {    
+
+    const movieModal = document.querySelector('.movie-details-modal')    
+    movieModal.classList.add('show')
+    
+
+    const detailsList = document.createElement('ul')
+    detailsList.classList.add('movie-details-list')
+
+    document.querySelector('.modal-content').append(detailsList)
+
+    const title = document.createElement('li')
+    title.innerHTML = '<strong>Title:</strong>' + data.Title
+    detailsList.append(title)
+
+    const year = document.createElement('li')
+    year.innerHTML = '<strong>Year:</strong>' + data.Year
+    detailsList.append(year)
+
+    const released = document.createElement('li')
+    released.innerHTML = '<strong>Released:</strong>' + data.Released
+    detailsList.append(released)
+
+    const runtime = document.createElement('li')
+    runtime.innerHTML = '<strong>Runtime:</strong>' + data.Runtime
+    detailsList.append(runtime)
+
+    const genre = document.createElement('li')
+    genre.innerHTML = '<strong>Genre:</strong>' + data.Genre
+    detailsList.append(genre)
+
+    const actors = document.createElement('li')
+    actors.innerHTML = '<strong>Actors:</strong>' + data.Actors
+    detailsList.append(actors)
+
+    const write = document.createElement('li')
+    write.innerHTML = '<strong>Write:</strong>' + data.Write
+    detailsList.append(write)
+
+    const plot = document.createElement('li')
+    plot.innerHTML = '<strong>Plot:</strong>' + data.Plot
+    detailsList.append(plot)
+}
+
+function closeMovieModal() {
+    const movieModal = document.querySelector('.movie-details-modal')
+    movieModal.classList.remove('show')
+
+    const movieModalList = document.querySelector('.movie-details-list')
+    movieModalList.remove()
 }
